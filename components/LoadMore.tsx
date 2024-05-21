ca3bd059fabdd2c9d2eed8f9756fcddfed4953ed
Anushka Gupta <anushkagupta319@example.com>
2024-05-21 12:21:21 +0530

    infinite scrolling done

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,16 @@
+"use client"; //hooks are not supported in server side rendering
 import Image from "next/image";
+import { useInView } from "react-intersection-observer";
 
 function LoadMore() {
+  const { ref, inView } = useInView(); //eliminates the need of next button, it will load more data when the user reaches the end of the page
+
   return (
     <>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
+          {" "}
+          {/* we will know when scroll , i.e through ref */}
           <Image //loader
             src="./spinner.svg"
             alt="spinner"
